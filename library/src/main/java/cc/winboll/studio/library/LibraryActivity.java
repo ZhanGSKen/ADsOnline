ea349c2e8e0702903bc364e452e560bfff9edbcf
ZhanGSKen <ZhanGSKen@QQ.COM>
2024-08-12 14:04:15 +0800

    新增WinBoll基础类库

---
+++ library/src/main/java/cc/winboll/studio/library/LibraryActivity.java
@@ -0,0 +1,14 @@
+package cc.winboll.studio.library;
+
+import android.app.*;
+import android.os.*;
+
+public class LibraryActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library);
+    }
+}
