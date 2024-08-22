158e129d3121964360b50e9f16412dc680d1249c
ZhanGSKen <ZhanGSKen@QQ.COM>
2024-08-23 03:42:38 +0800

    添加Stage调试版发布脚本

--- .winboll/bashCommitAppPublishBuildFlagInfo.sh
+++ .winboll/bashCommitAppPublishBuildFlagInfo.sh
@@ -2,7 +2,7 @@
 ## 提交新的 APK 编译配置标志信息，并推送到Git仓库。
 
 # 使用 `-z` 命令检查变量是否为空
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "Script parameter error: $0"
     exit 2
 fi
@@ -12,5 +12,5 @@ cd ${1}
 echo -e "Work dir : \n"`pwd`
 
 git add .
-git commit -m "APK Release ${2}"
+git commit -m "APK ${2} ${3} Publish."
 git push origin && git push origin --tags
